5bb4d523d2e4b3b49fff584826dbd6eb4ebc26e0
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2025-02-18 17:16:04 +0800

    refactor: update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import type { ChatData } from './types'
 import { onMounted, reactive, ref } from 'vue'
+import { toast } from 'vue3-toastify'
 import ChatGPTIcon from './components/ChatGPTIcon.vue'
+import SettingIcon from './components/SettingIcon.vue'
 import UserIcon from './components/UserIcon.vue'
 import { chat } from './lib/ai.ts'
+import 'vue3-toastify/dist/index.css'
 
 const messageContent = ref<string>('')
 const isConfig = ref<boolean>(true)
@@ -47,17 +50,25 @@ onMounted(() => {
 async function sendChatMessage(message: string) {
   const apiKey = getAPIKey()
   if (!apiKey) {
-    console.error('请先配置您的 API KEY')
+    toast.error('请先配置您的 API KEY', {
+      position: toast.POSITION.TOP_CENTER,
+    })
     return
   }
   const messageList = [
     { role: 'system', content: '你是一个 AI 助手，能够帮助用户解决各种问题' },
     { role: 'user', content: message },
   ]
-  const result = await chat(messageList, apiKey) as any
+  const result = await chat(messageList, apiKey)
+  if (!result) {
+    toast.error('获取数据失败，请检查您的 API KEY', {
+      position: toast.POSITION.TOP_CENTER,
+    })
+    return
+  }
   state.data.push({
     userAsk: message,
-    gptResponse: result.choices[0].message.content,
+    gptResponse: result,
   })
   clearMessageContent()
 }
@@ -80,12 +91,12 @@ async function sendMessageOrSaveAPIKey() {
 
 <template>
   <div class="flex flex-col h-screen pt-20">
-    <div class="flex flex-nowrap fixed w-full items-baseline top-0 px-6 py-4 bg-gray-100">
+    <div class="flex flex-nowrap fixed w-full items-center justify-between top-0 px-6 py-4 bg-gray-100">
       <div class="text-2xl font-bold">
         ChatGPT
       </div>
-      <div class="ml-auto px-3 py-2 text-sm cursor-pointer hover:bg-white rounded-md" @click="handleConfigButtonClick">
-        设置
+      <div class="p-2 cursor-pointer hover:bg-gray-200 rounded-md" @click="handleConfigButtonClick">
+        <SettingIcon :width="20" :height="20" />
       </div>
     </div>
     <div class="flex-1 ml-3">
@@ -104,17 +115,19 @@ async function sendMessageOrSaveAPIKey() {
         </div>
         <div class="flex mt-5">
           <ChatGPTIcon />
-          <div>{{ item.gptResponse }}</div>
+          <div class="ml-4 bg-gray-100 leading-10 h-10 rounded-md px-4">
+            {{ item.gptResponse }}
+          </div>
         </div>
       </div>
     </div>
-    <div class="sticky bottom-0 w-full p-6 pb-8 bg-gray-100 flex-shrink-0 flex-grow-0 basis-20">
-      <div v-if="isConfig" class="mb-2 text-sm text-gray-500">
-        请输入您的 API KEY
+    <div class="sticky bottom-0 w-full p-6 pb-8 bg-gray-100 flex flex-col">
+      <div v-if="isConfig" class="mb-2">
+        <a href="https://gpt302.saaslink.net/1BUaRS" target="_blank" class="text-sm text-gray-500 hover:text-blue-500">请输入您的 API KEY：</a>
       </div>
       <div class="flex">
-        <input v-model="messageContent" class="input" :type="isConfig ? 'password' : 'text'" @keydown.enter="sendMessageOrSaveAPIKey">
-        <button class="btn ml-4 hover:bg-white" @click="sendMessageOrSaveAPIKey">
+        <input v-model="messageContent" class="block w-96 rounded-md bg-white px-4 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" :type="isConfig ? 'password' : 'text'" @keydown.enter="sendMessageOrSaveAPIKey">
+        <button class="ml-4 rounded-md bg-indigo-600 px-4 py-2 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="sendMessageOrSaveAPIKey">
           {{ isConfig ? '保存' : '发送' }}
         </button>
       </div>
--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,7 +1,7 @@
 import type { ChatMessage } from '../types'
 
 export async function chat(messageList: ChatMessage[], apiKey: string) {
-  const result = await fetch('', {
+  const result = await fetch('https://api.302.ai/v1/chat/completions', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -12,5 +12,10 @@ export async function chat(messageList: ChatMessage[], apiKey: string) {
       message: messageList,
     }),
   })
-  return await result.json()
+  if (!result.ok) {
+    console.error('获取数据失败')
+    return null
+  }
+  const data = await result.json() as any
+  return data.choices[0].message.content as string
 }
